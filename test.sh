3a750c7c6f30904c014bee53f59f0c3f4547acc7
jalbiser <peliium.2022@gmail.com>
2024-05-06 22:43:55 +0200

    ok

--- test.sh
+++ test.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# Vérifier le nombre d'arguments
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <pid>"
-    exit 1
-fi
+# PID du serveur
+SERVER_PID="$1"
 
-# Récupérer le PID passé en argument
-pid="$1"
-
-# Lancer deux instances du client en arrière-plan avec le PID et les messages
-./client "$pid" "coucou c moi tchoupi"" &
-./client "$pid" "cje suis hariryuheb nje suis hariryuhebje suis hariryuhebje suis hariryuhebje suis hariryuheb" &
-
-# Attendre la fin de tous les processus en arrière-plan
+# Exécute deux instances du client simultanément avec des données aléatoires
+./client $SERVER_PID "coucou sdkf,ndsfknfdskjfdsnfdsfdsf essfdfdsfdsfds" & 
+./client $SERVER_PID "helllooo sdfknsdfjkdsfnj njdnfsdjnfdsjsdfnj ndjnsdfjnds" &
 wait
-
-echo "Les deux instances du client ont terminé l'exécution."
